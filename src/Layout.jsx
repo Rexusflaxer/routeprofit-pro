2cdfafb04ca76bd3e7269b047227caedfaa2a950
David Eijsvogel <davideijsvogel@gmail.com>
2026-02-20 12:12:02 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "./utils";
-import { LayoutDashboard, Users, Settings, Shield, Menu, X, CarFront, UserRound } from "lucide-react";
+import { LayoutDashboard, Users, Settings, Shield, Menu, X, CarFront } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 const NAV_ITEMS = [
